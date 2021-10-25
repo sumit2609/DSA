3c5460cce325091140e8c98398f8912aec78c3bd
Sumit Sharma <2609.shsumit@gmail.com>
2021-10-25 19:05:44 +0530

    Update Dijkstra's Algorithm.cpp

--- Graphs/Dijkstra's Algorithm.cpp
+++ Graphs/Dijkstra's Algorithm.cpp
@@ -1,6 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//Shortest path for weighted, directed graph and undirected graph,cyclic and ascyclic graph but with non negative weights.
+=======
+//Dijkstra algorithm tells shortest path from single source node to other node in graphs not necessary to be directed or undirected
 
 void dijkstraAlgorithm(int src, int n, vector<vector<pair<int,int>>> adj[]){
     vector<int> dis(n+1,INT_MAX);
